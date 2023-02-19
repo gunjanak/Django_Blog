a87575a28207f3e3e83d81df117212ca4a9d7cef
gunjanak <janak.klal@gmail.com>
2023-02-19 21:21:26 +0545

    seo friendly url

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,8 +6,13 @@ def post_list(request):
     posts = Post.published.all()
     return render(request,'blog/post/list.html',{'posts':posts})
 
-def post_detail(request,id):
-    post = get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+def post_detail(request,year,month,day,post):
+    print(year)
+    print(month)
+    print(day)
+    post = get_object_or_404(Post,
+    status=Post.Status.PUBLISHED,
+    slug=post)
 
     return render(request,'blog/post/detail.html',{'post':post})
 
